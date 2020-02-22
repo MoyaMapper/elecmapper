a0f06efb1b610c8ddc75b4b08a63656dd14f7cfe
LinXunFeng <598600855@qq.com>
2020-02-22 23:17:32 +0800

    集成format组件的reducer

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux-immutable';
 import { reducer as editReducer } from '../components/edit/store';
+import { reducer as formatReducer } from '../components/format/store';
 
 const reducer = combineReducers({
-  edit: editReducer
+  edit: editReducer,
+  format: formatReducer
 })
 
 export default reducer;
