a6f104b2c5a06850ca159f4923171403b4386db0
LinXunFeng <598600855@qq.com>
2020-02-22 23:16:25 +0800

    edit组件

--- src/components/edit/edit.js
+++ src/components/edit/edit.js
@@ -1,11 +1,86 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { connect } from 'react-redux';
+import { Input, Row, Col, Button, message } from 'antd';
+import jsonformat from '../../utils/json/jsonformat';
+import classNames from 'classnames';
+import { constants } from './store';
 import './edit.scss';
 
-const Edit = () => {
+const { TextArea } = Input;
+
+const Edit = (props) => {
+  let textArea = useRef(null)
+
+  const toFormat = () => {
+    const currentText = textArea.current.state.value
+    if (currentText == undefined || !currentText.length) {
+      message.warning('请先填写json数据');
+      return
+    }
+    // 格式化json数据
+    const value = jsonformat(currentText, false)
+    if (`${value}` == 'undefined') {
+      console.log('进来了')
+      message.error('无法格式化当前json数据');
+      return
+    }
+    
+    // 清空数据
+    textArea.current.setState({
+      value
+    })
+  }
+
+  const toTransform = () => {
+    console.log('转换')
+  }
+
+  const editClassName = classNames({
+    'editWrapper': true,
+    'fullshow': props.fullshow,
+    'hidden': props.hidden
+  })
   return (
-    <div className="editWrapper">我是Edit组件</div>
+    <div className={editClassName}>
+      <TextArea 
+        className="textArea"
+        placeholder="请填写json数据"
+        allowClear
+        ref={textArea}
+      />
+      <Row 
+        className="toolsRow"
+        type="flex" 
+        justify="space-around"
+      >
+        <Col span={10}>
+          <Button 
+            type="primary" 
+            block
+            onClick={toFormat}
+            className="toolsCol"
+          >
+            格式化
+          </Button>
+        </Col>
+        <Col span={10}>
+          <Button 
+            type="danger" 
+            block
+            onClick={toTransform}
+            className="toolsCol"
+          >
+            转换
+          </Button>
+        </Col>
+      </Row>
+    </div>
   )
 }
 
-export default connect(null, null)(Edit);
+const mapState = (state) => ({
+  fullshow: state.getIn(['edit', 'showType']) === constants.showType.full,
+  hidden: state.getIn(['edit', 'showType']) === constants.showType.hidden
+})
+
+export default connect(mapState, null)(Edit);
---
+++ src/components/edit/store/constants.js
@@ -0,0 +1,7 @@
+export let showType = {
+  hidden: 0,
+  half: 1,
+  full: 2
+};
+
+export const SWITCH_SHOW_TYPE = "edit/SWITCH_SHOW_TYPE";
