b96531890dbcad7c356960d7b1fd6b044e202079
LinXunFeng <598600855@qq.com>
2020-02-22 10:54:23 +0800

    默认不显示开发者工具

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ function createWindow() {
     // win.loadURL(`file://${path.join(__dirname, '../build/index.html')}`)  
     
     // 打开开发者工具
-    win.webContents.openDevTools()
+    // win.webContents.openDevTools()
   
     // 当 window 被关闭，这个事件会被触发。
     win.on('closed', () => {
