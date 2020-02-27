3c427f0a68b5a498646297b7b591a252337673a2
LinXunFeng <598600855@qq.com>
2020-02-27 13:23:50 +0800

    修改主页链接定义

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,6 +1,6 @@
 module.exports = {
   appName: "ElecMapper",
-  homepage: "https://github.com/MoyaMapper",
+  homepage: "https://github.com/MoyaMapper/elecmapper",
   organization: "MoyaMapper",
   organizationUrl: "https://github.com/MoyaMapper",
   author: "LinXunFeng",
